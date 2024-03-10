688d6173e8a72f86bc7c99800fcab75b1f0d4576
xdesign-williamvonarend <william.von-arend@xdesign.com>
2024-03-10 15:47:05 +0000

    add common and modules as spm packages

---
+++ CTrack-iOS/CTrack_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTrack_iOSApp.swift
+//  CTrack-iOS
+//
+//  Created by William von Arend on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CTrack_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
