c0ad05c2559e70915ef04c2a6caec541db293dd5
xdesign-williamvonarend <william.von-arend@xdesign.com>
2024-07-02 11:40:08 +0100

    add reusable network manager and error handling

--- Common/Sources/Services/CoinDataService.swift
+++ Common/Sources/Services/CoinDataService.swift
@@ -21,32 +21,16 @@ public class CoinDataService {
     
     private func getCoins() {
         guard let url = URL(string: // 2- Fetch the URL
-                                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+                            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
         )
         else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url) // 3- Get the data from the URL
-            .subscribe(on:DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse, // 4- Check it receives a valid response
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder()) // 5- It will decode the data into CoinModels
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription) // 6- If there's an error it will print it out
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins //7- If no error, it will append returnedCoins into allCoins
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
 
---
+++ Common/Sources/Services/Utils/NetworkingManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkingManager.swift
+//
+//
+//  Created by William von Arend on 01/07/2024.
+//
+
+import Combine
+import Foundation
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): "Bad response from URL: \(url)"
+            case .unknown: "Unknown error occured"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> { // By making it static we don't need to initialise the class. We can just call `NetworkingManager.download`
+        return URLSession.shared.dataTaskPublisher(for: url) // 3- Get the data from the URL
+            .subscribe(on:DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher() // Lets us simplify the type to `AnyPublisher<Data, Error>`
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse, // 4- Check it receives a valid response
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription) // 6- If there's an error it will print it out
+        }
+    }
+}
